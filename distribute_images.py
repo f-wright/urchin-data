75d35fb39740ce5e618eb8223e98990bcba464c1
f-wright <fwright@hmc.edu>
2024-03-01 15:24:43 -0800

    added image sampling for background and dealt with .ds_store bug

--- distribute_images.py
+++ distribute_images.py
@@ -4,12 +4,13 @@ from PIL import Image
 from pathlib import Path
 
 LABELERS = ["Brittany", "Castor", "Eliza", "Francine", "James", "Ryan"]
-INPUT_DIR = 'input_images'
-OUTPUT_DIR = 'images_to_label'
-FILE_CATEGORY = 'google_negative_1'
+INPUT_DIR = "input_images"
+OUTPUT_DIR = "images_to_label"
+FILE_CATEGORY = "background_lsui_0"
 CURRENT_DIR = os.getcwd()
 VIDEO_EXTENSIONS = {".mov", ".MOV"}
 IMAGE_EXTENSIONS = {".JPG", ".jpg", ".png", ".avif", ".webp", ".gif", ".jpeg"}
+
 
 def distribute_videos_and_images():
     # define inputs
@@ -17,7 +18,10 @@ def distribute_videos_and_images():
     files_list = os.listdir(input_path)
 
     # define outputs
-    output_dirs = [os.path.join(CURRENT_DIR, OUTPUT_DIR, labeler, FILE_CATEGORY) for labeler in LABELERS]
+    output_dirs = [
+        os.path.join(CURRENT_DIR, OUTPUT_DIR, labeler, FILE_CATEGORY)
+        for labeler in LABELERS
+    ]
     image_output_dirs = [os.path.join(labeler, "images") for labeler in output_dirs]
     video_output_dirs = [os.path.join(labeler, "videos") for labeler in output_dirs]
     num_groups = len(LABELERS)
@@ -29,12 +33,25 @@ def distribute_videos_and_images():
         Path(video_dir).mkdir(parents=True, exist_ok=True)
 
     # split images and videos
-    images_list = [file for file in files_list if (file[-5:] in IMAGE_EXTENSIONS or file[-4:] in IMAGE_EXTENSIONS)]
-    videos_list = [file for file in files_list if (file[file.index("."):] in VIDEO_EXTENSIONS)]
+    images_list = [
+        file
+        for file in files_list
+        if (file[-5:] in IMAGE_EXTENSIONS or file[-4:] in IMAGE_EXTENSIONS)
+    ]
+    videos_list = [
+        file for file in files_list if (file[file.index(".") :] in VIDEO_EXTENSIONS)
+    ]
 
     # make sure that all files are going to one of the folders
-    remaining_files = [file for file in files_list if (file not in images_list and file not in videos_list)]
-    assert len(remaining_files) == 0, f"Missing extension in the list of video and image extensions, please check all are included. Files not in either list: {remaining_files}"
+    remaining_files = [
+        file
+        for file in files_list
+        if (file not in images_list and file not in videos_list)
+        and (file != ".DS_Store")
+    ]
+    assert (
+        len(remaining_files) == 0
+    ), f"Missing extension in the list of video and image extensions, please check all are included. Files not in either list: {remaining_files}"
 
     # move images
     for path_i in range(len(images_list)):
@@ -42,12 +59,14 @@ def distribute_videos_and_images():
         original_path = os.path.join(input_path, images_list[path_i])
 
         # Convert to jpg and change file name to index
-        filename = str(path_i) + ".jpg"
+        filename = "new_" + str(path_i) + ".jpg"
         os.rename(original_path, os.path.join(input_path, filename))
         new_path = os.path.join(input_path, filename)
-        
+
         # Move image to labeler folder
-        shutil.move(new_path, os.path.join(image_output_dirs[path_i % num_groups], filename))
+        shutil.move(
+            new_path, os.path.join(image_output_dirs[path_i % num_groups], filename)
+        )
 
     # move videos
     for path_i in range(len(videos_list)):
@@ -58,9 +77,12 @@ def distribute_videos_and_images():
         filename = str(path_i) + ".MOV"
         os.rename(original_path, os.path.join(input_path, filename))
         new_path = os.path.join(input_path, filename)
-        
-        # Move video to labeler folder
-        shutil.move(new_path, os.path.join(video_output_dirs[path_i % num_groups], filename))
 
-if __name__ == '__main__':
+        # Move video to labeler folder
+        shutil.move(
+            new_path, os.path.join(video_output_dirs[path_i % num_groups], filename)
+        )
+
+
+if __name__ == "__main__":
     distribute_videos_and_images()
